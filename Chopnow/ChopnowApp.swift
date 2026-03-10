bcc43f9be050862a2566da27be47c1709fb1595f
Namubiru Rose <145221322+Rosevisuals@users.noreply.github.com>
2026-03-10 10:26:50 +0300

    Initial Commit

---
+++ Chopnow/ChopnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChopnowApp.swift
+//  Chopnow
+//
+//  Created by Rose Visuals on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChopnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chopnow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chopnow
+//
+//  Created by Rose Visuals on 10/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
